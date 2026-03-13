351caa8bbd8c2127d058afe43678a1a96b8ca9ba
chanchal <chanchal.2312051@gmail.com>
2026-03-13 18:28:45 +0530

    react-vite + tailwind and project structure setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='flex items-center justify-center h-screen bg-gray-900'>
+      <h1 className='text-4xl font-bold text-yellow-400'>
+        Tailwind works
+      </h1>
+    </div>   
+  )
+} 
+                
+              
+            
+   
+export default App
