9890c8d896c8cb1d48dc28c654e7a88d665dbdee
chanchal <chanchal.2312051@gmail.com>
2026-03-13 22:16:22 +0530

    persistent favourites using local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import "./index.css";
-import { useState, useReducer } from "react";
+import { useState, useReducer, useEffect } from "react";
 import Gallery from "./components/Gallery";
 import { favouritesReducer } from "./reducer/favouritesReducer";
 
 function App() {
   const [search, setSearch] = useState(" ");
-  const [favourites, dispatch] = useReducer(favouritesReducer, []);
+  const [favourites, dispatch] = useReducer(favouritesReducer, [],()=>{
+    const saved=localStorage.getItem("favourites")
+    return saved?JSON.parse(saved):[];
+  }
+);
+
+  useEffect(()=>{
+    localStorage.setItem("favourites",JSON.stringify(favourites));
+  },[favourites])
   return (
     <div className="min-h-screen bg-gray-100">
       <h1 className="text-3xl font-bold text-center py-6">Photo Gallery</h1>
