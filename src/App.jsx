b53fbd659e96dff044f91d27c3f417545b0db5d8
chanchal <chanchal.2312051@gmail.com>
2026-03-13 20:20:45 +0530

    added search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import './index.css'
+import { useState } from 'react';
 import Gallery from './components/Gallery'
 
 function App() {
+
+  const[search,setSearch]=useState(" ");
   return (
     <div className='min-h-screen bg-gray-100'>
       <h1 className='text-3xl font-bold text-center py-6'>
         Photo Gallery
       </h1>
-      <Gallery/>
+      <div className='flex justify-center mb-6'>
+        <input 
+        type="text" 
+        placeholder='Search by author...'
+        className='border p-2 rounded-lg w-80'
+        value={search}
+        onChange={(e)=>setSearch(e.target.value)}
+        />
+      </div>
+      <Gallery search={search}/>
     </div>   
   );
 }      
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,14 +1,18 @@
-import React from "react";
 import useFetchPhotos from "../hooks/useFetchPhotos";
 import PhotoCard from "./PhotoCard";
 
-function Gallery() {
+function Gallery({ search }) {
   const { photos, loading, error } = useFetchPhotos();
   if (loading) return <p className="text-center mt-10">Loading...</p>;
   if (error) return <p className="text-center text-red-500">{error}</p>;
+
+  const filteredPhotos = photos.filter((photo) =>
+    photo.author.toLowerCase().includes(search.toLowerCase()),
+  );
+
   return (
     <div className="p-6 grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
-      {photos.map((photo) => (
+      {filteredPhotos.map((photo) => (
         <PhotoCard key={photo.id} photo={photo} />
       ))}
     </div>
