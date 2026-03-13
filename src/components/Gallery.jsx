c406ea586179d4ad69a248a15bcd9262e74de635
chanchal <chanchal.2312051@gmail.com>
2026-03-13 22:29:30 +0530

    assignment completed

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,22 +1,31 @@
+import { useMemo } from "react";
 import useFetchPhotos from "../hooks/useFetchPhotos";
 import PhotoCard from "./PhotoCard";
 
-function Gallery({ search, favourites,dispatch }) {
+function Gallery({ search, favourites, dispatch }) {
   const { photos, loading, error } = useFetchPhotos();
+
+  const filteredPhotos = useMemo(() => {
+    return photos.filter((photo) =>
+      photo.author.toLowerCase().includes(search.toLowerCase())
+    );
+  }, [photos, search]);
+
   if (loading) return <p className="text-center mt-10">Loading...</p>;
   if (error) return <p className="text-center text-red-500">{error}</p>;
-
-  const filteredPhotos = photos.filter((photo) =>
-    photo.author.toLowerCase().includes(search.toLowerCase()),
-  );
 
   return (
     <div className="p-6 grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
       {filteredPhotos.map((photo) => (
-        <PhotoCard key={photo.id} photo={photo} favourites={favourites} dispatch={dispatch}/>
+        <PhotoCard
+          key={photo.id}
+          photo={photo}
+          favourites={favourites}
+          dispatch={dispatch}
+        />
       ))}
     </div>
   );
 }
 
-export default Gallery;
+export default Gallery
